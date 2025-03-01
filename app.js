a132f4f9d8a6715093a0605acc8469e9acb22881
benjaminb <benbasseri@gmail.com>
2025-03-01 11:41:44 -0500

    factoring out routing

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app = express();
-app.use((req, res, next) => {
-  console.log('in first mw function');
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
+// Catchall route
 app.use((req, res, next) => {
-  console.log('in second mw function');
-  res.send('<h1>Testing</h1>');
+  res.status(404).send('<h1>Error 404</h1><p><h2>Page not found</h2></p>');
 });
 
 app.listen(3000);
