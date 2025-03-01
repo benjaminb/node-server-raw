790d8582d5525265fb49aae86c46d1ce26780672
benjaminb <benbasseri@gmail.com>
2025-03-01 10:36:09 -0500

    sets up express usage

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const http = require('http');
-const routeHandler = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routeHandler);
+app = express();
+app.use((req, res, next) => {
+  console.log('in first mw function');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('in second mw function');
+});
+
+const server = http.createServer(app);
 server.listen(3000);
