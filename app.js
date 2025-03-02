d3f89183c092191494579fc5884228a112e4c82c
benjaminb <benbasseri@gmail.com>
2025-03-02 17:27:47 -0500

    making files generic

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const frontendRoutes = require('./routes/frontend');
 
 app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use(frontendRoutes);
 
 // Catchall route
 app.use((req, res, next) => {
